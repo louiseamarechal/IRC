230fc0ad0991b3589f20076ea1690bca8cb8327d
louisea <louiseamelie2@gmail.com>
2023-02-09 14:52:36 +0100

    seems ok - Jacinthe to approve

--- srcs/user/User.cpp
+++ srcs/user/User.cpp
@@ -121,14 +121,21 @@ void User::handleCommand(std::string buffer)
 
     std::cout << "Handle Command -- Buffer = " << buffer << std::endl;
    
-
-    while(buffer.size() != 0)
-    { 
-        std::cout << "Buffer size = " << buffer.size() << std::endl;
+    std::cout << "Buffer size = " << buffer.size() << std::endl;
     std::cout << "pos  = " << pos << std::endl;
-        pos = buffer.find(whitespace);// retourne premier espace trouve
-        endlinepos = buffer.find("\n");// retourne premier \n trouve
-        s1 = buffer.substr(0, pos); // copie la commande
+
+    pos = buffer.find(whitespace);// retourne premier espace trouve
+    endlinepos = buffer.find("\n");// retourne premier \n trouve
+    s1 = buffer.substr(0, pos); // copie la commande
+    
+    if (pos == std::string::npos)
+    {
+        s1.clear();
+        s1 = buffer.substr(0, buffer.size() - 2); // remove \r\n
+        s2 = "";
+    }
+    else
+    {
         pos++;
         s2 = buffer.substr(pos, endlinepos - pos - 1);
         if (endlinepos != buffer.size())
@@ -137,9 +144,9 @@ void User::handleCommand(std::string buffer)
             buffer = tmp;
         }
         pos = s2.size();
-        std::cout << "Handle Command -- Command = " << s1 << std::endl;
-        std::cout << "Handle Command -- Params = " << s2 <<std::endl;
-        if (getServer()->getCommandMap().count(s1) > 0)
-            getServer()->getCommandMap()[s1](s2, *this);
     }
+    std::cout << "Handle Command -- Command = " << s1 << std::endl;
+    std::cout << "Handle Command -- Params = " << s2 <<std::endl;
+    if (getServer()->getCommandMap().count(s1) > 0)
+        getServer()->getCommandMap()[s1](s2, *this);
 }
