3f8140ebde3ce76fc1f3b2ece661c8a18322247a
louisea <louiseamelie2@gmail.com>
2023-03-02 11:43:20 +0100

    ping ok

--- srcs/commands/PING.cpp
+++ srcs/commands/PING.cpp
@@ -3,9 +3,9 @@
 
 void ping(std::string params, User &user)
 {
-    std::string tmp  =  " PONG :" + params + "\r\n";
+    std::string tmp  =  ":" + user.getServer()->getServerName() + " PONG :" + params + "\r\n";
 	if (params.size() == 0)
-		sendMessage(409, user, *user.getServer());
+		tmp = sendMessage(409, user, *user.getServer());
 	send(user.getUserFd(), tmp.c_str(), tmp.size(),0);
     
 }
